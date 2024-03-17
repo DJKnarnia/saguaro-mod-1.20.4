67569dfbbeb903f8b10af60b88d5ad7773e8505d
DJKnarnia <djknarnia@yahoo.com>
2024-03-17 18:54:18 -0400

    Initial push

---
+++ src/main/java/net/djknarnia/saguaro/SaguaroModClient.java
@@ -0,0 +1,10 @@
+package net.djknarnia.saguaro;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SaguaroModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/djknarnia/saguaro/SaguaroModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.djknarnia.saguaro;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SaguaroModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
